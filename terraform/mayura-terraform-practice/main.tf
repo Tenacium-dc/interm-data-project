5944218fe9ce4ea00a2dccc2814ef887485a7479
Sen Mayur <mayurapiriyan.ramasamy@tenacium.co.uk>
2023-12-13 17:08:24 +0000

    Updated main.tf

--- terraform/mayura-terraform-practice/main.tf
+++ terraform/mayura-terraform-practice/main.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-    type = string
-}
-
-variable "secret_key" {
-    type = string
-}
-
 variable "vpc_security_group_ids" {
     type = list(string)
 }
@@ -19,13 +11,11 @@ variable "ec2_instance_key" {
 }
 
 terraform {
-  backend "s3" {}
-}
-
-provider "aws" {
+  backend "s3" {
+    bucket = "mayura-terraform-state"
+    key = "terraform-state"
     region = "eu-west-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+  }
 }
 
 resource "aws_instance" "mayura_terraform_test" {
