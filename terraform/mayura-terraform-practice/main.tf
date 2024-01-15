c05b23efbe853be0386f1bca6710f31c5d8c4305
Sen Mayur <mayurapiriyan.ramasamy@tenacium.co.uk>
2024-01-15 18:27:28 +0000

    Reverting back to original configuration

--- terraform/mayura-terraform-practice/main.tf
+++ terraform/mayura-terraform-practice/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "mayura_terraform_test" {
     ami = "ami-0694d931cee176e7d"
     vpc_security_group_ids = var.vpc_security_group_ids
     subnet_id = var.subnet_id
-    instance_type = "t2.small"   
+    instance_type = "t2.micro"   
     availability_zone = "eu-west-1c"
     key_name = var.ec2_instance_key
     associate_public_ip_address = true
